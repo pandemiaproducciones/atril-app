3518d04f7b584e1120ccaac03e881e5499103d1c
pandemiaproducciones <paisatuba@gmail.com>
2026-02-05 11:09:59 -0500

    Update service worker cache version to v82

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'atril-v801-cache';
+const CACHE_NAME = 'atril-v82-cache';
 const urlsToCache = [
   './',
   './index.html',
@@ -53,6 +53,7 @@ self.addEventListener('activate', event => {
   );
 
 });
+
 
 
 
