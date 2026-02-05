63fd7bbf43496a7ef3daa59d3f87221d57953fc1
pandemiaproducciones <paisatuba@gmail.com>
2026-02-05 11:43:12 -0500

    Add service worker for caching and fetch handling

---
+++ sw-beta.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'atril-v84-beta-cache';
+const urlsToCache = [
+  './beta.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
